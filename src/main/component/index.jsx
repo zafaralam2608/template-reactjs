ffeb5475abd503b4b7c466d1b06280e9e77e07c1
Md Zafar Alam <zafaralam2608@gmail.com>
2022-09-13 19:35:00 +0530

    drawer

--- src/main/component/index.jsx
+++ src/main/component/index.jsx
@@ -1,14 +1,7 @@
 import React, { useMemo, useState } from 'react';
-import {
-  HashRouter, Route, Routes,
-} from 'react-router-dom';
-import {
-  createTheme, CssBaseline, ThemeProvider, Toolbar,
-} from '@mui/material';
-import Header from './Header';
+import { createTheme, ThemeProvider } from '@mui/material';
 import { ColorModeContext } from '../constant/context';
-import Dashboard from './Dashboard';
-import Welcome from './Welcome';
+import Layout from './Layout';
 
 function App() {
   const [darkMode, setDarkMode] = useState(true);
@@ -34,15 +27,7 @@ function App() {
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Header />
-        <Toolbar />
-        <HashRouter>
-          <Routes>
-            <Route index element={<Dashboard />} />
-            <Route path="welcome" element={<Welcome />} />
-          </Routes>
-        </HashRouter>
+        <Layout />
       </ThemeProvider>
     </ColorModeContext.Provider>
   );
